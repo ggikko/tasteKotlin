afc0b7ceadc165ac3be7848961068384cfa452bd
Jihong Park <ggikko@hotmail.com>
2016-08-02 16:37:11 +0900

    add junit

---
+++ KotlinExample/app/src/test/java/ggikko/me/kotlinexample/KotlinTest.kt
@@ -0,0 +1,18 @@
+package ggikko.me.kotlinexample
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Created by ggikko on 16. 8. 2..
+ */
+
+class KotlinJunitTest {
+
+    @Test
+    fun firstTest() {
+        Assert.assertTrue(1 == 1)
+    }
+
+}
+
